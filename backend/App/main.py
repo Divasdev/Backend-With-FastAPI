8efd77656a7fc670bcee78fc9237b7b403b1dddb
Divasdev <sharmadivas881@gmail.com>
2026-09-16 14:08:44 +0530

    feat: implement Snip model with created_at field and database initialization on startup

--- backend/App/main.py
+++ backend/App/main.py
@@ -9,10 +9,53 @@ from fastapi.encoders import jsonable_encoder
 from typing import Annotated
 from sqlmodel import Field,Session,SQLModel,create_engine,select 
 
-
+from datetime import date
 
 sqlite_file_name="database.db"
 sqlite_url=f"sqlite:///{sqlite_file_name}"
 
 engine=create_engine(sqlite_url,connect_args={"check_same_thread":False})
 
+
+class SnipBase(SQLModel):
+   title:str
+   language:str 
+   code:str 
+   description:str 
+   
+class Snip(SnipBase,table=True):
+   id:int|None=Field(default=None,primary_key=True)
+   created_at:date=Field(default_factory=date.today)
+   
+class SnipCreate(SnipBase):
+   pass 
+class SnipPublic(SnipBase):
+    id: int
+    created_at: date
+   
+   
+   
+def create_db_and_tables():
+   SQLModel.metadata.create_all(engine)
+      
+
+def get_session():
+   with Session(engine) as session:
+      yield session
+      
+SessionDep=Annotated[Session,Depends(get_session)]
+
+app=FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+   create_db_and_tables()
+
+@app.post("/snippets/",response_model=SnipPublic)
+
+def create_snip(snip:SnipCreate,session:SessionDep):
+   db_snip=Snip.model_validate(snip)
+   session.add(db_snip)
+   session.commit()
+   session.refresh(db_snip)
+   return db_snip
